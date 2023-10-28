885cbebc8784594b1ddc9e78fc00b74b0a756cdb
Ariel Salcito <ariel_salcito@live.com>
2023-10-28 11:43:43 -0300

    Update-app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,20 +7,66 @@ const TURNS = {
 
 const Square = ({ children, isSelected, updateBoard, index }) => {
   const className = `square ${isSelected ? "is-selected" : ""}`;
+
   const handleClick = () => {
     updateBoard(index)
   }
 
-  return <div onClick={handleClick} className={className}>{children}</div>;
+  return (
+    <div onClick={handleClick} className={className}>
+      {children}
+    </div>
+    );
 };
 
-function App() {
-  const [board, setBoard] = useState(Array(9).fill(null));
-  const [turn, setTurn] = useState(TURNS.X);
+const WINNER_COMBOS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8]
+]
 
-  const updateBoard = () => {
+function App() {
+
+  const [board, setBoard] = useState(Array(9).fill(null));
+
+  const [turn, setTurn] = useState(TURNS.X);
+  const [winner, setWinner] = useState(null) // null = no hay ganador, false = empate;
+  const checkWinner = (boardToCheck) => {
+    
+    for ( const combo of WINNER_COMBOS){
+      const [a,b,c] = combo
+      if(boardToCheck[a] &&
+        boardToCheck[a] === boardToCheck[b] &&
+        boardToCheck[a] === boardToCheck[c]
+        )
+        {
+          return boardToCheck[a]
+        }
+      }
+      return null
+    }
+
+  const updateBoard = (index) => {
+
+    if(board[index] || winner) return;
+
+    const newBoard = [...board]
+
+    newBoard[index] = turn
+
+    setBoard(newBoard)
+
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
+
+    const newWinner = checkWinner(newBoard)
+    if(newWinner){
+      setWinner(newWinner)
+    }
+
   }
 
   return (
@@ -30,7 +76,7 @@ function App() {
         {board.map((e, index) => {
           return (
             <Square key={index} index={index} updateBoard={updateBoard}>
-              {index}
+              {board[index]}
             </Square>
           );
         })}
