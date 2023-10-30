c0ea3fef3ae66e1027fa82385d77ee1649ea25d3
Ariel Salcito <ariel_salcito@live.com>
2023-10-29 22:57:32 -0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,34 @@
 import { useState } from "react";
-import confetti from 'canvas-confetti'
+import confetti from "canvas-confetti";
 
 import { Square } from "./components/Square.jsx";
-import { TURNS} from './constants.js'
-import { checkWinnerFrom, checkEndGame} from "./logic/board.js";
+import { TURNS } from "./constants.js";
+import { checkWinnerFrom, checkEndGame } from "./logic/board.js";
 import { WinnerModal } from "./components/WinnerModal.jsx";
 
 function App() {
-
   const [board, setBoard] = useState(() => {
-    const boardFromStorage = window.localStorage.getItem('board');
-    return boardFromStorage ? JSON.parse(boardFromStorage) : Array(9).fill(null)
+    const boardFromStorage = window.localStorage.getItem("board");
+    return boardFromStorage
+      ? JSON.parse(boardFromStorage)
+      : Array(9).fill(null);
   });
 
   const [turn, setTurn] = useState(() => {
-    const turnFromStorage = window.localStorage.getItem('turn')
-    return turnFromStorage ?? TURNS.X
+    const turnFromStorage = window.localStorage.getItem("turn");
+    return turnFromStorage ?? TURNS.X;
   });
 
   const [winner, setWinner] = useState(null); // null = no hay ganador, false = empate;
 
   const resetGame = () => {
-    setBoard(Array(9).fill(null))
-    setTurn(TURNS.X)
-    setWinner(null)
+    setBoard(Array(9).fill(null));
+    setTurn(TURNS.X);
+    setWinner(null);
 
-    window.localStorage.removeItem('board')
-    window.localStorage.removeItem('turn')
-  }
+    window.localStorage.removeItem("board");
+    window.localStorage.removeItem("turn");
+  };
 
   const updateBoard = (index) => {
     if (board[index] || winner) return;
@@ -41,15 +42,15 @@ function App() {
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
     setTurn(newTurn);
 
-    window.localStorage.setItem('board', JSON.stringify(newBoard))
-    window.localStorage.setItem('turn', newTurn)
+    window.localStorage.setItem("board", JSON.stringify(newBoard));
+    window.localStorage.setItem("turn", newTurn);
 
     const newWinner = checkWinnerFrom(newBoard);
     if (newWinner) {
-      confetti()
+      confetti();
       setWinner(newWinner);
-    } else if (checkEndGame(newBoard)){
-      setWinner(false)
+    } else if (checkEndGame(newBoard)) {
+      setWinner(false);
     }
   };
 
